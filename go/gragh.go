df0fcf2a6b681d70c99645a3ce6506c86ffed6dc
shota-aa <dqx1005@icloud.com>
2022-01-30 15:59:01 +0900

    split more

--- go/gragh.go
+++ go/gragh.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
 )
 
 // グラフのデータ点を一日分生成
@@ -170,4 +173,61 @@ func calculateGraphDataPoint(isuConditions []IsuCondition) (GraphDataPoint, erro
 		},
 	}
 	return dataPoint, nil
+}
+
+// GET /api/isu/:jia_isu_uuid/graph
+// ISUのコンディショングラフ描画のための情報を取得
+func getIsuGraph(c echo.Context) error {
+	jiaUserID, errStatusCode, err := getUserIDFromSession(c)
+	if err != nil {
+		if errStatusCode == http.StatusUnauthorized {
+			return c.String(http.StatusUnauthorized, "you are not signed in")
+		}
+
+		c.Logger().Error(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	jiaIsuUUID := c.Param("jia_isu_uuid")
+	datetimeStr := c.QueryParam("datetime")
+	if datetimeStr == "" {
+		return c.String(http.StatusBadRequest, "missing: datetime")
+	}
+	datetimeInt64, err := strconv.ParseInt(datetimeStr, 10, 64)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "bad format: datetime")
+	}
+	date := time.Unix(datetimeInt64, 0).Truncate(time.Hour)
+
+	tx, err := db.Beginx()
+	if err != nil {
+		c.Logger().Errorf("db error: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	defer tx.Rollback()
+
+	var count int
+	err = tx.Get(&count, "SELECT COUNT(*) FROM `isu` WHERE `jia_user_id` = ? AND `jia_isu_uuid` = ?",
+		jiaUserID, jiaIsuUUID)
+	if err != nil {
+		c.Logger().Errorf("db error: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	if count == 0 {
+		return c.String(http.StatusNotFound, "not found: isu")
+	}
+
+	res, err := generateIsuGraphResponse(tx, jiaIsuUUID, date)
+	if err != nil {
+		c.Logger().Error(err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		c.Logger().Errorf("db error: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	return c.JSON(http.StatusOK, res)
 }
--- go/isu.go
+++ go/isu.go
@@ -7,8 +7,6 @@ import (
 	"errors"
 	"io/ioutil"
 	"net/http"
-	"strconv"
-	"time"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
@@ -295,61 +293,4 @@ func getIsuIcon(c echo.Context) error {
 	}
 
 	return c.Blob(http.StatusOK, "", image)
-}
-
-// GET /api/isu/:jia_isu_uuid/graph
-// ISUのコンディショングラフ描画のための情報を取得
-func getIsuGraph(c echo.Context) error {
-	jiaUserID, errStatusCode, err := getUserIDFromSession(c)
-	if err != nil {
-		if errStatusCode == http.StatusUnauthorized {
-			return c.String(http.StatusUnauthorized, "you are not signed in")
-		}
-
-		c.Logger().Error(err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-
-	jiaIsuUUID := c.Param("jia_isu_uuid")
-	datetimeStr := c.QueryParam("datetime")
-	if datetimeStr == "" {
-		return c.String(http.StatusBadRequest, "missing: datetime")
-	}
-	datetimeInt64, err := strconv.ParseInt(datetimeStr, 10, 64)
-	if err != nil {
-		return c.String(http.StatusBadRequest, "bad format: datetime")
-	}
-	date := time.Unix(datetimeInt64, 0).Truncate(time.Hour)
-
-	tx, err := db.Beginx()
-	if err != nil {
-		c.Logger().Errorf("db error: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-	defer tx.Rollback()
-
-	var count int
-	err = tx.Get(&count, "SELECT COUNT(*) FROM `isu` WHERE `jia_user_id` = ? AND `jia_isu_uuid` = ?",
-		jiaUserID, jiaIsuUUID)
-	if err != nil {
-		c.Logger().Errorf("db error: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-	if count == 0 {
-		return c.String(http.StatusNotFound, "not found: isu")
-	}
-
-	res, err := generateIsuGraphResponse(tx, jiaIsuUUID, date)
-	if err != nil {
-		c.Logger().Error(err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		c.Logger().Errorf("db error: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-
-	return c.JSON(http.StatusOK, res)
 }
